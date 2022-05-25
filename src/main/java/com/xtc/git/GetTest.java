aaa30799123324e4d6d043b8bad8f4eba056f874
xtc <2385969457@qq.com>
2022-05-25 18:03:56 +0800

    first commit

---
+++ src/main/java/com/xtc/git/GetTest.java
@@ -0,0 +1,11 @@
+package com.xtc.git;
+
+/**
+ * @author 小天才
+ * @create 2022-05-25-17:59
+ */
+public class GetTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git !!!");
+    }
+}
